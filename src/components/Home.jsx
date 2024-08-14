88196d8326ac9334d122086be155115d3cf36e3d
Nipun Pramodya Jayasinghe <nipunprjay@gmail.com>
2024-08-14 14:57:20 +0530

    Google sign in implemented

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,45 +1,9 @@
-import firebase from "../firebase";
-// import { useContext } from 'react';
-// import AuthContext from './AuthContext';
+// import React from 'react'
 
+const Home = () => {
+  return (
+    <div>Home</div>
+  )
+}
 
-const signUp = (email, password) => {
-  firebase.auth().createUserWithEmailAndPassword(email, password)
-    .then((userCredential) => {
-      // Handle successful sign-up
-      console.log('User signed up successfully', userCredential.user);
-    })
-    .catch((error) => {
-      // Handle errors
-      console.error('Error during sign-up:', error.message);
-      alert(`Sign-up failed: ${error.message}`);
-    });
-};
-
-const signIn = (email, password) => {
-  firebase.auth().signInWithEmailAndPassword(email, password)
-    .then((userCredential) => {
-      // Handle successful sign-in
-      console.log('User signed in successfully', userCredential.user);
-    })
-    .catch((error) => {
-      // Handle errors
-      console.error('Error during sign-in:', error.message);
-      alert(`Sign-in failed: ${error.message}`);
-    });
-};
-
-const signOut = () => {
-  firebase.auth().signOut()
-    .then(() => {
-      // Handle successful sign-out
-      console.log('User signed out successfully');
-    })
-    .catch((error) => {
-      // Handle errors
-      console.error('Error during sign-out:', error.message);
-      alert(`Sign-out failed: ${error.message}`);
-    });
-};
-
-export { signUp, signIn, signOut };
+export default Home
--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,14 @@
-import { StrictMode } from 'react'
+import React from 'react';
+// import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
+import { GoogleOAuthProvider } from "@react-oauth/google"
 
 createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
+  <GoogleOAuthProvider clientId='786774659320-c9jobogegja4qkmg9cj0vbl6o9794ql9.apps.googleusercontent.com'>
+    <React.StrictMode>
+      <App />
+    </React.StrictMode>
+  </GoogleOAuthProvider>
 )
